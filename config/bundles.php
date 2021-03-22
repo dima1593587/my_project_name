4d54a656edd87c151f3475677c2da0c37bf970b5
dima1593587 <79845080+dima1593587@users.noreply.github.com>
2021-03-22 21:43:00 +0300

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
