82f437b23361924f99bf5b4a57b3b61ba56c2657
dima1593587 <79845080+dima1593587@users.noreply.github.com>
2021-03-27 15:02:36 +0300

    Update PageController.php

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,6 +1,4 @@
 <?php
-
-
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +8,6 @@ class PageController extends AbstractController
 {
     public function page()
     {
-              return $this->render('Mypage/mypage.html.twig'
-			 );
+        return $this->render('mypage/mypage.html.twig');
     }
 }
